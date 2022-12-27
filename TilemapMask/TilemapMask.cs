3d37f78ae469ee18c282d1521c140b15c03a895e
Andrew <109795959+JustAnCore@users.noreply.github.com>
2022-12-27 07:09:43 +0300

    Init commit

---
+++ TilemapMask/TilemapMask.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapMask : MonoBehaviour
+{
+    //-- Made by @JustAnCore
+    //-- License: Apache 2.0
+
+    public GameObject maskCell;
+
+    private void Start()
+    {
+        Tilemap tilemap = GetComponent<Tilemap>();
+
+        Vector3Int startCoord = tilemap.origin;
+        Vector3Int size = tilemap.size;
+
+        //Iterate over each cell
+        for (int x = startCoord.x; x < startCoord.x + size.x; x++)
+        {
+            for (int y = startCoord.y; y < startCoord.y + size.y; y++)
+            {
+                //Check if cell isn't empty
+                if (tilemap.GetTile(new Vector3Int(x, y, startCoord.z)) != null)
+                {
+                    //Create maskCell on the cell coords
+                    Vector3 coord = tilemap.CellToWorld(new Vector3Int(x, y, startCoord.z)) + new Vector3(0.5f, 0.5f, 0);
+                    Instantiate(maskCell, coord, Quaternion.identity, transform);
+                }
+            }
+        }
+    }
+}
